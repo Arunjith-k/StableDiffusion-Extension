2d72161304fed33a0de2ff7e6d5645ba38ee2e3f
Hari_dev <97119967+Airox1221@users.noreply.github.com>
2025-01-27 19:30:05 +0530

    Delete content.js

--- content.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("mousedown", (event) => {
-  if (event.button === 2) {
-    // Right-click
-    const img = event.target.closest("img");
-    if (img) {
-      chrome.storage.local.set({ selectedImage: img.src });
-    }
-  }
-});
